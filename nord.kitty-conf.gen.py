f70bb286425294ee025e1211eb49ee9665d9f492
Malik BEN KIRANE <free.malikbenkirane@gmail.com>
2018-07-28 02:33:18 +0200

    fix nord.kitty-conf.gen.py fileype

--- nord.kitty-conf.gen.py
+++ nord.kitty-conf.gen.py
@@ -1,1 +1,55 @@
-../nord.kitty-conf.gen.py
+# ! Copyright (c) 2016-present Arctic Ice Studio <development@arcticicestudio.com>
+# ! Copyright (c) 2016-present Sven Greb <code@svengreb.de>
+# 
+# ! Project:    Nord XResources
+# ! Version:    0.1.0
+# ! Repository: https://github.com/arcticicestudio/nord-xresources
+# ! License:    MIT
+# ! Ported by:  git@github.com:malikbenkirane
+
+# #define   nord0    #2E3440
+# #define   nord1    #3B4252
+# #define   nord2    #434C5E
+# #define   nord3    #4C566A
+# #define   nord4    #D8DEE9
+# #define   nord5    #E5E9F0
+# #define   nord6    #ECEFF4
+# #define   nord7    #8FBCBB
+# #define   nord8    #88C0D0
+# #define   nord9    #81A1C1
+# #define   nord10   #5E81AC
+# #define   nord11   #BF616A
+# #define   nord12   #D08770
+# #define   nord13   #EBCB8B
+# #define   nord14   #A3BE8C
+# #define   nord15   #B48EAD
+
+names = ['nord0', 'nord1', 'nord2', 'nord3', 'nord4', 'nord5', 'nord6', 'nord7',
+ 'nord8', 'nord9', 'nord10', 'nord11', 'nord12', 'nord13', 'nord14', 'nord15']
+
+colors = ['#2E3440', '#3B4252', '#434C5E', '#4C566A', '#D8DEE9', '#E5E9F0', '#ECEFF4',
+ '#8FBCBB', '#88C0D0', '#81A1C1', '#5E81AC', '#BF616A', '#D08770', '#EBCB8B',
+ '#A3BE8C', '#B48EAD']
+
+# *fading: 35
+# *fadeColor: nord3
+
+print( 'foreground    ' + colors[names.index('nord4')] )
+print( 'background    ' + colors[names.index('nord0')] )
+print( 'cursorColor   ' + colors[names.index('nord4')] )
+print( 'color0        ' + colors[names.index('nord1')] )
+print( 'color1        ' + colors[names.index('nord11')] )
+print( 'color2        ' + colors[names.index('nord14')] )
+print( 'color3        ' + colors[names.index('nord13')] )
+print( 'color4        ' + colors[names.index('nord9')] )
+print( 'color5        ' + colors[names.index('nord15')] )
+print( 'color6        ' + colors[names.index('nord8')] )
+print( 'color7        ' + colors[names.index('nord5')] )
+print( 'color8        ' + colors[names.index('nord3')] )
+print( 'color9        ' + colors[names.index('nord11')] )
+print( 'color10       ' + colors[names.index('nord14')] )
+print( 'color11       ' + colors[names.index('nord13')] )
+print( 'color12       ' + colors[names.index('nord9')] )
+print( 'color13       ' + colors[names.index('nord15')] )
+print( 'color14       ' + colors[names.index('nord7')] )
+print( 'color15       ' + colors[names.index('nord6')] )
